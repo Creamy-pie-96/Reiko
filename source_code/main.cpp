eb722dc7e490af73aef7ab3521a62eebbbcb0be3
Md. Nasif Sadik Prithu <nasifsadik9@gmail.com>
2025-09-14 20:58:48 +0600

    Clean up main.cpp by removing unnecessary comments
    Removed comments to clean up the code and improve readability.

--- source_code/main.cpp
+++ source_code/main.cpp
@@ -9,7 +9,6 @@ using namespace std;
 int main() {
     cout << "Welcome to REIKO, your personal emotion tracker!" << endl;
 
-    // Call preload to initialize global variables
     preload();
 
     vector<string> user_input;
@@ -17,26 +16,20 @@ int main() {
 
     cout << "Please enter your sentences (type 'exit' to quit):" << endl;
 
-    // Take user input until "exit" is entered
     while (true) {
         getline(cin, input_line);
 
-        // Check if the user wants to exit
         if (input_line == "exit") {
             break;
         }
 
-        // Add the input line to the user_input vector
         user_input.push_back(input_line);
     }
 
-    // Add end-of-input marker to indicate the end of user input
     user_input.push_back("__EnD_Of_InPuT_123!@#__");
 
-    // Process the input to fix typos and generate responses
     fixTypo(user_input);
 
-    // Trigger learning before exiting
     analyzeTrackedData();
     saveTrackedData("../mechine knowledge/Potential emotion.txt", "../mechine knowledge/Potential intensifier.txt");
 
